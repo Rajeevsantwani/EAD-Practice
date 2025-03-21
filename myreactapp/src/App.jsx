bc348ab3999d8b7ef24ee97ea022a9da255ff957
Rajeevsantwani <rajeevsantwani575@gmail.com>
2025-03-21 20:45:55 +0500

    Add new file and changes in APP

--- myreactapp/src/App.jsx
+++ myreactapp/src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-
+import Form from './Form'
 function App() {
 
   return (
     <>
-      <div className="container">
-        <h1>Rajeev</h1>
-        </div>     
+        <Form />
     </>
   )
 }
---
+++ myreactapp/src/Form.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Form() {
+  return (
+    <div>
+    <h1>Form</h1>
+        <div className="inner"> 
+            <label>Username</label>
+            <input type="text" />
+            <br />
+            <label>Password</label>
+            <input type="text" />
+            <br />
+            
+      </div>
+    </div>
+  )
+}
